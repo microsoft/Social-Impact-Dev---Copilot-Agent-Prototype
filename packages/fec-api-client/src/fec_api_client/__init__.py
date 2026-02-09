ffbb29476f5104612f328fa4ddfc59e9982c858e
Keith Fung <keithrfung@users.noreply.github.com>
2026-02-09 15:15:39 -0500

    Add ReportTypeCode type for FEC report filtering (#14)

--- packages/fec-api-client/src/fec_api_client/__init__.py
+++ packages/fec-api-client/src/fec_api_client/__init__.py
@@ -1,3 +1,6 @@
 from .generated.fec_openapi import FecOpenapiAPIs as FecApiClient
+from .generated.fec_openapi.models import *  # noqa: F401, F403
+from .generated.fec_openapi.models import __all__ as _models_all
+from .types import ReportTypeCode
 
-__all__ = ["FecApiClient"]
+__all__ = ["FecApiClient", "ReportTypeCode", *_models_all]
